316b13f0255b10d6f16238c3d2913431884bd571
gaesa <71256557+gaesa@users.noreply.github.com>
2025-03-18 13:47:07 +0000

    feat: notify when download skipped

--- downloader/download_image.go
+++ downloader/download_image.go
@@ -2,6 +2,7 @@ package downloader
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path"
 
@@ -21,6 +22,7 @@ func DownloadImage(url string, dirPath string) error {
 		// 获取文件大小
 		fileSize := fileInfo.Size()
 		if fileSize > 0 {
+			log.Printf("图片 %s 已存在，跳过下载。\n", imgName)
 			return nil
 		}
 	}
