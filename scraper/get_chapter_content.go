ed0c577b0ceefac87314722b8af243e2dc81d373
gaesa <71256557+gaesa@users.noreply.github.com>
2025-03-19 10:35:33 +0000

    refactor: add abstraction to `GetChapterContent`
    - Extracted the Android-specific content fetching and cleaning logic
    into the `fetchAndroidContent` function.
    - Added a helper function `isContentFetched` to check if content is
    fetched successfully.

--- scraper/get_chapter_content.go
+++ scraper/get_chapter_content.go
@@ -7,34 +7,42 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+func getChapterContentAndroid(chapter *Chapter) error {
+	doc, err := AndroidGet(chapter.Url)
+	if err != nil {
+		return err
+	}
+
+	content := doc.Find("body").Text()
+
+	content = strings.ReplaceAll(content, "&nbsp;", "")
+	content = strings.ReplaceAll(content, "更多精彩热门日本轻小说、动漫小说，轻小说文库(http://www.wenku8.com) 为你一网打尽！", "")
+
+	picReg := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg`)
+	picRegL := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg\([0-9]+K\)`)
+	images := picReg.FindAllString(content, -1)
+	content = picRegL.ReplaceAllString(content, "")
+	content = picReg.ReplaceAllString(content, "")
+
+	chapterContent := &ChapterContent{
+		Images:  images,
+		Article: content,
+	}
+	chapter.Content = chapterContent
+	return nil
+}
+
+func isContentFetched(doc *goquery.Document) bool {
+	return strings.TrimSpace(doc.Find("#contentmain span").First().Text()) != "null"
+}
+
 func GetChapterContent(chapter *Chapter) error {
 	doc, err := Get(chapter.Url)
 	if err != nil {
 		return err
 	}
-	if strings.TrimSpace(doc.Find("#contentmain span").First().Text()) == "null" {
-		content := ""
-		doc, err := AndroidGet(chapter.Url)
-		if err != nil {
-			return err
-		}
-		content = doc.Find("body").Text()
-
-		content = strings.ReplaceAll(content, "&nbsp;", "")
-		content = strings.ReplaceAll(content, "更多精彩热门日本轻小说、动漫小说，轻小说文库(http://www.wenku8.com) 为你一网打尽！", "")
-
-		picReg := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg`)
-		picRegL := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg\([0-9]+K\)`)
-		images := picReg.FindAllString(content, -1)
-		content = picRegL.ReplaceAllString(content, "")
-		content = picReg.ReplaceAllString(content, "")
-
-		chapterContent := &ChapterContent{
-			Images:  images,
-			Article: content,
-		}
-		chapter.Content = chapterContent
-		return nil
+	if !isContentFetched(doc) {
+		return getChapterContentAndroid(chapter)
 	}
 
 	content := doc.Find("#content").Text()
