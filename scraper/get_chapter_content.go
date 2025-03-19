4a3087b7f5449ebf90293c350a5b15d0266910a0
gaesa <71256557+gaesa@users.noreply.github.com>
2025-03-19 09:59:43 +0000

    refactor: prefer `ReplaceAll` than `Replace` with `-1`

--- scraper/get_chapter_content.go
+++ scraper/get_chapter_content.go
@@ -20,8 +20,8 @@ func GetChapterContent(chapter *Chapter) error {
 		}
 		content = doc.Find("body").Text()
 
-		content = strings.Replace(content, "&nbsp;", "", -1)
-		content = strings.Replace(content, "更多精彩热门日本轻小说、动漫小说，轻小说文库(http://www.wenku8.com) 为你一网打尽！", "", -1)
+		content = strings.ReplaceAll(content, "&nbsp;", "")
+		content = strings.ReplaceAll(content, "更多精彩热门日本轻小说、动漫小说，轻小说文库(http://www.wenku8.com) 为你一网打尽！", "")
 
 		picReg := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg`)
 		picRegL := regexp.MustCompile(`http:\/\/pic\.wenku8\.com\/pictures\/[\/0-9]+.jpg\([0-9]+K\)`)
@@ -38,9 +38,9 @@ func GetChapterContent(chapter *Chapter) error {
 	}
 
 	content := doc.Find("#content").Text()
-	content = strings.Replace(content, "本文来自 轻小说文库(http://www.wenku8.com)", "", -1)
-	content = strings.Replace(content, "台版 转自 轻之国度", "", -1)
-	content = strings.Replace(content, "最新最全的日本动漫轻小说 轻小说文库(http://www.wenku8.com) 为你一网打尽！", "", -1)
+	content = strings.ReplaceAll(content, "本文来自 轻小说文库(http://www.wenku8.com)", "")
+	content = strings.ReplaceAll(content, "台版 转自 轻之国度", "")
+	content = strings.ReplaceAll(content, "最新最全的日本动漫轻小说 轻小说文库(http://www.wenku8.com) 为你一网打尽！", "")
 
 	images := []string{}
 	doc.Find("img").Each(func(i int, imgEle *goquery.Selection) {
