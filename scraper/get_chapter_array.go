525dab665ca4275966383e4b54b9aef0b343bead
gaesa <71256557+gaesa@users.noreply.github.com>
2025-03-19 09:52:06 +0000

    refactor: prefer `url.Parse` & `ResolveReference` than `strings.ReplaceAll`

--- scraper/get_chapter_array.go
+++ scraper/get_chapter_array.go
@@ -1,7 +1,8 @@
 package scraper
 
 import (
-	"strings"
+	"log"
+	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -18,15 +19,33 @@ func GetChapterArray(volume *Volume) ([]*Chapter, error) {
 }
 
 func insertMap(rows *goquery.Selection, chapterArray *[]*Chapter, start int, end int, volumeName string, catalogueUrl string) {
+	baseUrl, err := url.Parse(catalogueUrl)
+	if err != nil {
+		log.Printf("Error: Invalid catalogue URL %s: %v", catalogueUrl, err)
+		return
+	}
+
 	rows.Slice(start, end).Find("a").Each(func(i int, s *goquery.Selection) {
 		chapterIndex := i + 1
 		chapterTitle := s.Text()
-		chapterUrl, _ := s.Attr("href")
-		chapterUrl = strings.ReplaceAll(catalogueUrl, "index.htm", chapterUrl)
+
+		chapterUrl, exists := s.Attr("href")
+		if !exists {
+			log.Printf("Warning: Missing chapter URL in volume %s, skipping entry %d", volumeName, chapterIndex)
+			return
+		}
+
+		parsedChaperUrl, err := url.Parse(chapterUrl)
+		if err != nil {
+			log.Printf("Error: Invalid chapter URL %s in volume %s: %v", chapterUrl, volumeName, err)
+			return
+		}
+		fullUrl := baseUrl.ResolveReference(parsedChaperUrl)
+
 		chapter := &Chapter{
 			Index: chapterIndex,
 			Title: chapterTitle,
-			Url:   chapterUrl,
+			Url:   fullUrl.String(),
 		}
 		*chapterArray = append(*chapterArray, chapter)
 	})
